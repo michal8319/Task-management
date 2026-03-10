c96b055c50d1da7c05d0a2d09b19643ea7643df5
Nechami kolman <email@example.com>
2026-03-10 02:13:31 +0200

    night monday uploude update

---
+++ Project-and-task-management/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+
+
+const Home=()=>{
+   return(
+    <>
+    <Button endDecorator={<KeyboardArrowRight />} color="success">
+     Go to checkout
+    </Button>
+    </>
+   )
+}
+export default Home 
---
+++ Project-and-task-management/src/store/ProjectIdSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const ProjectIdSlice = createSlice({
+  name: "projectId",
+  initialState: null,
+  reducers: {
+    setProjectId: (state, action) => {
+      return action.payload;
+    }
+  }
+});
+export const { setProjectId } = ProjectIdSlice.actions;
+export default ProjectIdSlice.reducer;
